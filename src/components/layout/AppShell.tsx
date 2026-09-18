f6def678cec567bc61a889c539d7eb5f823cc471
AzureAD\MilosRadulovic <milos_radulovic@epam.com>
2026-09-18 14:11:37 +0200

    Button visibility updated

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -5,7 +5,7 @@ import { Topbar } from '@/components/layout/Topbar'
 
 export function AppShell() {
   return (
-    <div className="flex min-h-svh bg-bg text-text">
+    <div className="flex h-svh bg-bg text-text">
       <Sidebar />
       <div className="flex min-w-0 flex-1 flex-col">
         <Topbar />
